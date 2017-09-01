dd8df690cd02abb2fe3e8794979a570076a29678
danclegg <dgclegg@gmail.com>
2017-09-01 15:50:58 -0600

    Copying binaries to dist

--- dist/RMC3/copyFromSrc.ps1
+++
@@ -1,7 +0,0 @@
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\*.lpz" -Destination "c:\repos\crestron-tec-lite\dist\RMC3"
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\*.sig" -Destination "c:\repos\crestron-tec-lite\dist\RMC3"
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\*.hash" -Destination "c:\repos\crestron-tec-lite\dist\RMC3"
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\*.vtz" -Destination "c:\repos\crestron-tec-lite\dist\RMC3"
-
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\*TSW-750.vtz" -Destination "c:\repos\crestron-tec-lite\dist\Touchpanels\TSW-750"
-Copy-Item -Path "c:\repos\crestron-tec-lite\src\RMC3\Other Displays\*FT-TS600.vtz" -Destination "C:\repos\crestron-tec-lite\dist\Touchpanels\FT-TS600"
